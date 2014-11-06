23c24eabd6923b8b3b7558ca58e34aa6fedd0076
Bryan Dent <bryan@dent.ly>
2014-11-06 13:41:40 -0500

    Options Dialog example

--- tampa/2014-11-06/UIControls/app/controllers/index.js
+++ tampa/2014-11-06/UIControls/app/controllers/index.js
@@ -6,16 +6,32 @@
 var App = require("core");
 
 // Make sure we always have a reference to the navigation window for iOS
-if(OS_IOS) {
-	App.navigationWindow = $.navWindow;
-	App.navigationWindow.open();
+if (OS_IOS) {
+  App.navigationWindow = $.navWindow;
+  App.navigationWindow.open();
 } else {
-	$.indexWindow.open();
+  $.indexWindow.open();
 }
 
 // Init our app singleton
 App.init();
 
-$.list.addEventListener("itemclick", function(e){
-	App.Navigator.open(e.itemId);
+$.list.addEventListener("itemclick", function(e) {
+  if (e.itemId === 'OptionsDialog') {
+    showOptions();
+  } else {
+    App.Navigator.open(e.itemId);
+  }
 });
+
+function showOptions() {
+  var opts = {
+    cancel: 2,
+    options: ['Confirm', 'Help', 'Cancel'],
+    selectedIndex: 2,
+    destructive: 0,
+    title: 'Delete File?'
+  };
+
+  var dialog = Ti.UI.createOptionDialog(opts).show();
+}
